85140dce725449f540a9f22fe84b269e073064ab
Krista Blachian <KristaBlachian@outlook.com>
2018-02-07 21:28:47 -0500

    vice breaker first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//main server code
+
+const express = require('express');
+const mustache = require('mustache-express');
+const app = express();
+
+app.engine('mustache', mustache());
+app.set('view engine', 'mustache');
+
+app.get('/', function (req, res){
+    res.render('index');
+});
+
+app.listen(3000);
